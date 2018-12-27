a6654b72f665c039fa4f30dbf5f3308bbe703822
souppower <r.kubota92@gmail.com>
2018-12-27 22:12:53 +0900

    :recycle: jestのdiagnosticsを切った

--- jest.config.js
+++ jest.config.js
@@ -57,7 +57,8 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   globals: {
     "ts-jest": {
-      tsConfig: "tsconfig.json"
+      tsConfig: "tsconfig.json",
+      diagnostics: false
     }
   },
 
