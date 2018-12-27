07826f102746c73ef1890b96104689126a22df5a
souppower <r.kubota92@gmail.com>
2018-12-27 23:20:24 +0900

    :recycle: jestでnon relative importができるようにした

--- jest.config.js
+++ jest.config.js
@@ -76,6 +76,8 @@ module.exports = {
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
 
+  modulePaths: ["<rootDir>/src/"],
+
   // Activates notifications for test results
   // notify: false,
 
