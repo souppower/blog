0d17ec98fd9eeb4ee15e484accad5a90bf8536d1
souppower <r.kubota92@gmail.com>
2018-12-30 21:23:28 +0900

    :shower: mockファイルをテストファイルに統合

--- src/interface/repository/article/index.spec.ts
+++ src/interface/repository/article/index.spec.ts
@@ -1,5 +1,26 @@
 import ArticleRepository from "./index";
-import API, { mockArticle } from "interface/repository/mocks/api";
+
+export const mockArticle = {
+  id: "id",
+  title: "title",
+  body: "body",
+  published: new Date()
+};
+
+class API {
+  get(id: string) {
+    return mockArticle;
+  }
+  post() {
+    return mockArticle;
+  }
+  put() {
+    return mockArticle;
+  }
+  delete(id: string) {
+    return null;
+  }
+}
 
 describe("Article Repository", () => {
   const repo = new ArticleRepository(new API());
